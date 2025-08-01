e9b5d8f121350845274ef900ac5fd3bf9ce243f6
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-02 01:44:59 +0200

    [UPD] fine-esercizio-base

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const app = express();
 // definisco la porta
 const port = 3000;
 
-//
+//importo il file router dei post
+const postRouter = require("./routers/posts");
 
 // definisco la rotta base
 app.get("/", (req, res) => {
   res.send("Benvenuti nel mio blog");
 });
+
+// definisco la rotta posts
+app.use("/posts", postRouter);
 
 // dico al server di rimanere in ascolto alla porta 3000
 app.listen(port, () => {
