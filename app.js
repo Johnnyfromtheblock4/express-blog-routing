9f1ec2d447ce77538b3f843f19c8940b794460b9
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-04 10:22:06 +0200

    [UPD] es-correzione

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ const port = 3000;
 //importo il file router dei post
 const postRouter = require("./routers/posts");
 
+// inserisco il middleware uso use perchè voglio ottenerlo
+app.use(express.static("public"));
+
+// definisco la rotta posts
+app.use("/posts", postRouter);
+
 // definisco la rotta base
 app.get("/", (req, res) => {
   res.send("Benvenuti nel mio blog");
 });
-
-// definisco la rotta posts
-app.use("/posts", postRouter);
 
 // dico al server di rimanere in ascolto alla porta 3000
 app.listen(port, () => {
